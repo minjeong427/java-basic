90896552cd83a08fc49f29622709da47c014359a
minjeong427 <bling427@naver.com>
2024-02-26 17:36:27 +0900

    # Java oop(오버로딩, 객체배열)

---
+++ src/oop/encap/bad/MainClass.java
@@ -0,0 +1,14 @@
+package oop.encap.bad;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        MyBirth my = new MyBirth();
+        my.year = 1992;
+        my.month = 10;
+        my.day = -13515;
+
+        my.birthInfo();
+
+    }
+}
